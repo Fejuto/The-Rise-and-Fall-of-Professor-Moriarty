fc11900359c44dba0bada918276bd009d16d1b1d
Fedor <team@orbitgametech.com>
2012-12-17 11:11:17 +0100

    wander fix

--- source/rise/MonsterC.hx
+++ source/rise/MonsterC.hx
@@ -150,7 +150,7 @@ class MonsterC extends C{
 	}
 	
 	function wander(td:Float):Void {
-
+		if(e.destroyed) return;
 	  	var point = U.pointOnEdgeOfCircle(nodeC.x, nodeC.y, Config.NodeStartRadius + 20, td);
 	  	x = point[0];
 	  	y = point[1] + monsterBounceY;
